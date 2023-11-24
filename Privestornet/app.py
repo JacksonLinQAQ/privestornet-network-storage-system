015b3771eaa0865ac2603a5d1bc5db8a5516bcd3
jacksonlin7 <150607336+jacksonlin7@users.noreply.github.com>
2023-11-24 17:50:55 +0800

    updated

--- Privestornet/app.py
+++ Privestornet/app.py
@@ -1,3 +1,3 @@
-from Privestornet.PSNApplication.PSNApplication import APP_CONFIG, PSN_APP
+from Privestornet.PSNApplication.PSNApplication import APP_CONFIG, PSN_APP, PSN_SYS
 
 PSN_APP.run(APP_CONFIG['host'], APP_CONFIG['port'])
